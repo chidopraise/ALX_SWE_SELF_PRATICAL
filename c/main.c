e7655158343010b67d999876fe7b5c863af2f393
Abraham <abrahamlivinus@gmail.com>
2023-10-26 10:08:16 -0700

    ALX_SWE_SELF_PRATICAL/c

---
+++ c/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check code
+ *
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+	wake();
+	return (0);
+}
---
+++ c/wake.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * @here i want to write a function that will print
+ * hello world for the first time on my own.
+ *
+ * now i will print it and also complile it, to print
+ * the output on my sand box. returning 0
+ */
+
+char wake()
+{
+	char str[] = "Hello World, i just wrote my first c programming and i'm aspiring to become the best software engineer.";
+
+	printf("%s\n",str);
+}
